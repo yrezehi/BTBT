8a2f2214ad1a5ad245c570b57b36e82ef54b1acd
unknown <yrezehi@gmail.com>
2023-11-01 18:30:47 -0700

    setup: PeerInstance

---
+++ btbt/src/main/java/io/decentralized/btbt/peers/PeerInstance.java
@@ -0,0 +1,16 @@
+package main.java.io.decentralized.btbt.peers;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class PeerInstance {
+    UUID Identifier;
+    public String Name;
+    public Date Joined;
+
+    public PeerInstance(String name) {
+        Name = name;
+        Identifier = UUID.randomUUID();
+        Joined = new Date();
+    }
+}
